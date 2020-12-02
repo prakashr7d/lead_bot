40b1d0dc652a3563adb506dbf963b78d3144d041
prakashr7d <prakashr7d@github.com>
2020-12-02 15:58:45 +0530

    second

---
+++ actions/actions.py
@@ -0,0 +1,21 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from . import connector
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name = tracker.get_slot("name")
+        email = tracker.get_slot("email")
+        number = tracker.get_slot("phone-number")
+        connector.user_commit(name,email,number)
+        dispatcher.utter_message(text=f"Thanks for jumping into our website {name}, our costumer support employee will contact you")
+
+        return []
---
+++ actions/connector.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import math, random 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="prakash",
+  password="Forfacebook*1",
+  database = "lead_bot")
+
+mycursor= mydb.cursor()
+
+def user_commit(name, phone_number, email):
+    mycursor.execute(f"insert into user(name,email_id,phone_number) values('{name}', '{phone_number}', '{email}')")
+    mydb.commit()
